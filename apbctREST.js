dc25eff834f672122de7a4f4afb46dfe5dbd2e73
Glomberg <bazz@bk.ru>
2022-09-06 19:56:59 +0300

    Fix. Rest class arguments fixed.

--- apbctREST.js
+++ apbctREST.js
@@ -1,13 +1,14 @@
 class ApbctRest extends ApbctXhr{
 
-    default_route = ctPublicFunctions._rest_url + 'cleantalk-antispam/v1/';
+    static default_route = ctPublicFunctions._rest_url + 'cleantalk-antispam/v1/';
     route         = '';
 
     constructor(...args) {
-
-        super(...args);
-
-        args.url = this.default_route + args.route;
-        args.headers['X-WP-Nonce'] = ctPublicFunctions._rest_nonce;
+        args = args[0];
+        args.url = ApbctRest.default_route + args.route;
+        args.headers = {
+            "X-WP-Nonce": ctPublicFunctions._rest_nonce
+        };
+        super(args);
     }
 }
